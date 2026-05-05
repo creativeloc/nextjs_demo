116ff6e3df02e9d573ae3b9ad4cd571aa31a59d9
Lawrence Caulker <locaulker@gmail.com>
2026-05-05 14:38:35 -0400

    reviewed basic routing, nested and dynamic routing in nextjs

--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -1,6 +1,8 @@
-const UserDetails = ({params: Promise<{id:string}>}) => {
+const UserDetails = async ({params}:  {params: Promise<{ id: string }>}) => {
+  const {id} = await params;
+
   return (
-      <div>UserDetails</div>
+      <h1>showing details for user #{id}</h1>
   )
 }
 export default UserDetails
--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -7,10 +7,10 @@ const Users = () => {
         <h1>Dashboard Users</h1>
 
         <ul className="mt-10">
-          <li><Link href="/dashboard/users/1">User 1</Link></li>
-          <li><Link href="/dashboard/users/2">User 2</Link></li>
-          <li><Link href="/dashboard/users/3">User 3</Link></li>
-          <li><Link href="/dashboard/users/4">User 4</Link></li>
+          <li className='text-5xl'><Link href="/dashboard/users/1">User 1</Link></li>
+          <li className='text-5xl'><Link href="/dashboard/users/2">User 2</Link></li>
+          <li className='text-5xl'><Link href="/dashboard/users/3">User 3</Link></li>
+          <li className='text-5xl'><Link href="/dashboard/users/4">User 4</Link></li>
         </ul>
       </div>
   )
